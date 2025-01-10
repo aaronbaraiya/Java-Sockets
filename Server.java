ae9551e5e59b8c7a25cf5a1c016eadb1e12c4c68
aaronbaraiya <39784741+aaronbaraiya@users.noreply.github.com>
2025-01-10 12:35:20 -0800

    Create Server.java

---
+++ Server.java
@@ -0,0 +1,41 @@
+package net.sockets.simplified;
+
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    private ServerSocket server;
+    public static final int PORT = 3030;
+    public static final String STOP_STRING = "##";
+    private int index = 0;
+
+    public Server(){
+        try{
+            server = new ServerSocket(PORT);
+            while(true) iniConnections();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void iniConnections() throws IOException {
+        Socket clientSocket = server.accept();
+
+        if(clientSocket.isConnected())
+        new Thread(()->{
+            index++;
+            ConnectedClient client = new ConnectedClient(clientSocket,index);
+            client.readMessages();
+            client.close();
+        }).start();
+    }
+
+
+    public static void main(String[] args) {
+        new Server();
+    }
+}
